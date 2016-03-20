d237a13b80f2a6caded0abb116e35badffdd1c98
megrela <g.megreli@gmail.com>
2016-03-20 12:55:56 +0200

    update and remove logic added(not tested yet)

--- application/modules/component_groups/router.py
+++ application/modules/component_groups/router.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, session, redirect, url_for
 from application.mongo_db import mongo
+from application.constants import constants
 
 from . import module
 from .setup import setup
@@ -20,3 +21,30 @@ def add():
         "group_id": request.form.get("group_id")
     })
     return json.dumps({"id": str(res.inserted_id)})
+
+
+@module.route('/delete', methods=("POST",))
+def remove():
+    mongo.db.component_groups.remove({
+        "_id": {"$oid": request.form.get("id")}
+    })
+    return ""
+
+
+@module.route('/update', methods=("POST",))
+def update():
+    name = request.form.get("name")
+    group_id = request.form.get("group_id")
+    oid = request.form.get("id")
+    mongo.db.component_groups.update(
+        {
+            "_id": {"$oid": oid}
+        },
+        {
+            "$set": {
+                "name": name,
+                "group_id": group_id
+            }
+        }
+    )
+    return ""
