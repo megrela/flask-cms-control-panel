5de655de65c8ca2388f4f6fc654f94972ea52584
gime <Giorgi.Megreli@betssongroup.com>
2016-03-21 16:39:28 +0200

    add update ad remove routes added

--- application/modules/components/router.py
+++ application/modules/components/router.py
@@ -2,7 +2,6 @@ from flask import render_template, request, session, redirect, url_for
 from application.mongo_db import mongo
 
 
-
 import json
 from bson.objectid import ObjectId
 
@@ -18,7 +17,7 @@ def index(component_type):
         kwargs["dependencies"] = setup(component_type)
         return render_template("components/%s.html" % component_type, **kwargs)
     else:
-        return ""
+        return "", 404
 
 
 @module.route("/<component_type>/filter", methods=("POST",))
@@ -26,7 +25,7 @@ def index(component_type):
     if validation.validate_type(component_type):
         result = list(
             mongo.db.components.find({
-                "group_id": request.form.get("group_id")
+                "group_id": ObjectId(request.form.get("group_id"))
             })
         )
         return json.dumps(result)
@@ -34,4 +33,39 @@ def index(component_type):
         return json.dumps({}), 404
 
 
+@module.route("/<component_type>/add", methods=("POST",))
+def delete(component_type):
+    if validation.validate_type(component_type):
+        result = list(
+            mongo.db.components.find({
+                "group_id": ObjectId(request.form.get("group_id"))
+            })
+        )
+        return json.dumps(result)
+    else:
+        return json.dumps({}), 404
+
+
+@module.route("/<component_type>/remove", methods=("POST",))
+def delete(component_type):
+    if validation.validate_type(component_type):
+        mongo.db.components.remove({
+            "_id": ObjectId(request.form.get("id"))
+        })
+        return json.dumps({})
+    else:
+        return json.dumps({}), 404
+
+
+@module.route("/<component_type>/update", methods=("POST",))
+def delete(component_type):
+    if validation.validate_type(component_type):
+        result = list(
+            mongo.db.components.find({
+                "group_id": ObjectId(request.form.get("group_id"))
+            })
+        )
+        return json.dumps(result)
+    else:
+        return json.dumps({}), 404
 
